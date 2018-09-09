11f6988364a3ce2f56e63b25f4a00e2e5dca7b38
Katarzyna Lukaszuk <lukaszuk.kasia@gmail.com>
2018-09-09 14:18:05 +0100

    integrated dropbox upload

--- capture.py
+++ capture.py
@@ -3,25 +3,55 @@ from datetime import datetime
 import logging
 import os
 from time import sleep
+
 from picamera import PiCamera
+import toml
+import dropbox
+
+with open('config.toml', 'r') as fo:
+    config = toml.load(fo)
 
 LOG_PATH = '/home/pi/timelapse/log/camera.log'
 logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG)
 
 IMAGE_PATH = '/home/pi/timelapse/images'
+DBOX_DESTINATION = '/Apps/Raspberry Pie Camera'
+
+
+def send_to_dropbox(fpath, destination):
+	'''
+	Send a file to Dropbox
+	'''
+
+	with open(fpath, 'rb') as fo:
+		data = fo.read()
+
+	fname = os.path.basename(fpath)
+
+	try:
+		res = dbx.files_upload(data, '{}/{}'.format(destination, fname))
+	except dropbox.exceptions.ApiError as e:
+		logging.error(str(e))
 
 
 if __name__ == '__main__':
+	
+	dbx = dropbox.Dropbox(config['dropbox']['ACCESS_TOKEN'])
 	
 	with  PiCamera() as camera:
 		
 		camera.resolution = (1280, 720)
 		camera.brightness = 50
+		camera.zoom = (0.0, 0.0, 1.0, 1.0)
+
 		while True:
 			dt = datetime.now().isoformat()
-			fname = os.path.join(IMAGE_PATH, 'img_{}.jpg'.format(dt))
+			fpath = os.path.join(IMAGE_PATH, 'img_{}.jpg'.format(dt))
 			camera.annotate_text = dt
-			camera.capture(output=fname)
-			logging.info('saving {}'.format(fname))
+			camera.capture(output=fpath)
+			logging.info('saving {}'.format(fpath))
+			logging.info('sending to dropbox ...')
+			send_to_dropbox(fpath, DBOX_DESTINATION)
+			logging.info('sending to dropbox done.')
 			sleep(30)
 
